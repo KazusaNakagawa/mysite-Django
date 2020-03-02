6ab36422a12ee56c63ca1049ed0d6111cf89c63e
Kazusa Nakagawa <kazusanakgawa@gmail.com>
2020-03-02 23:55:05 +0900

    Create url config

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from blog.views import IndexView, PostDetailView, CategoryListView, TagListView
+
+app_name = 'blog'
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('categorise/', CategoryListView.as_view(), name='category_list'),
+    path('tags/', TagListView.as_view(), name='tag_list'),
+
+]
