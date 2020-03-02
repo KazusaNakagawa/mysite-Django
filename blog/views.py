231dcdae6ba2f995d2b01524662363fc919d1f97
Kazusa Nakagawa <kazusanakgawa@gmail.com>
2020-03-02 23:47:04 +0900

    create View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.db.models import Count, Q
+from django.http import Http404
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from blog.models import Category
+from blog.models import Post
+from blog.models import Tag
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if not obj.is_public and not self.request.user.is_authenticated:
+            raise Http404
+        return obj
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index_html'
+
+
+class CategoryListView(ListView):
+    queryset = Category.objects.annotate(
+        num_posts=Count('post', filter=Q(post__is_public=True)))
+
+
+class TagListView(ListView):
+    queryset = Tag.objects.annotate(
+        'post', filter=Q(post__is_public=True))
