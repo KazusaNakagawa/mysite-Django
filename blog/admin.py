c29d81fa6ea5556eeafd1f16a262ef2790292644
Kazusa Nakagawa <kazusanakgawa@gmail.com>
2020-03-03 21:38:52 +0900

    models.py: Insert images into blog posts. admin.py: Use admin's "inline" feature to allow editing from the parent model page.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-from blog.models import Category, Post, Tag
+from blog.models import Category, ContentImage, Post, Tag
+
+
+class ContentImageInline(admin.TabularInline):
+    """ Use admin's "inline" feature to allow editing from the parent model page."""
+    model = ContentImage
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        ContentImageInline,
+    ]
+
 
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
