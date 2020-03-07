8e1fbb70b9e10a8f2e131680d6ab8b001cdf4204
Kazusa Nakagawa <kazusanakgawa@gmail.com>
2020-03-07 16:16:17 +0900

    delete source

--- dbsite/urls.py
+++ dbsite/urls.py
@@ -20,9 +20,6 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-from blog.views import SearchPostView
-
-# app_name = 'blog'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
