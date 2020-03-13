36fea7783bfd4142df2a8de2d59a6c93a6e7bc3e
Kazusa Nakagawa <kazusanakgawa@gmail.com>
2020-03-14 08:50:52 +0900

    cookie save: judge dark-theme or light-theme

--- blog/static/blog/js/mode.js
+++ blog/static/blog/js/mode.js
@@ -6,10 +6,22 @@ btn.addEventListener("change", () => {
   if (btn.checked == true) {
     // ダークモード
     document.body.classList.remove("light-theme");
-    document.body.classList.add("dark-theme");
+    var dark_theme = document.body.classList.add("dark-theme");
   } else {
     // ライトモード
     document.body.classList.remove("dark-theme");
     document.body.classList.add("light-theme");
   }
+  setCookie();
+
 });
+
+// cookie保存
+function setCookie() {
+  myCookieVal = $('body').hasClass('dark-theme')? 'isActive':'notActive';
+  $.cookie('myCookieName', myCookieVal, { expires: 365, path: '/' })
+}
+
+// Cookieから情報を読み込み
+var myCookieName = $.cookie("myCookieName");
+console.log(myCookieName);
