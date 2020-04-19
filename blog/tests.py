a9e5048beb8991ff66b2e9065459ae2a33f49587
Kazusa Nakagawa <kazusanakgawa@gmail.com>
2020-04-19 13:52:47 +0900

    ブログ名をblog

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
+# from .models import Category
+from .models import Comment
+from .models import Post
+
+
 # Create your tests here.
+
+class PostTests(TestCase):
+    def test_save(self):
+        save = Post()
+        self.assert_(True, msg=save)
+
+    def test_save_false(self, *args, **kwargs):
+        save = Post(*args, **kwargs)
+        with self.assertRaises(AssertionError):
+            self.assert_(False, msg=save)
+
+    def test_save_request(self):
+        save = Post()
+        str_ = self.__str__().isalnum()
+        self.assertNotEqual(save, str_, msg='save')
+
+
+class CommentTests(TestCase):
+    def setUp(self) -> None:
+        print('setup')
+        self.comment = Comment()
+
+    def tearDown(self) -> None:
+        print('clean up')
+        del self.comment
+
